c7ef701536d2acbc1d5c9297cd7265d572c04e0f
Elixir <nathanael.martin210918@gmail.com>
2023-01-07 19:36:13 +0100

    Modifier EntryPoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,6 +40,10 @@ else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
+cd /home/container/garrysmod
+git pull
+cd /home/container
+
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
