321d70ec2daf679e28f94508ba97ce57566bb850
Elixir <nathanael.martin210918@gmail.com>
2023-01-07 20:06:34 +0100

    Modified EntryPoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,8 @@ else
 fi
 
 cd /home/container/garrysmod
-git pull ${GITHUB_BRANCH}
+git branch ${GITHUB_BRANCH}
+git pull
 cd /home/container
 
 # Display the command we're running in the output, and then execute it with the env
